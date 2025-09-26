7e8bf8e586408bf87d628d4f43b7c4895fdecc26
yu-2-tang <1926651882@qq.com>
2025-09-26 15:23:18 +0800

    update hello to NEU

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,5 +6,4 @@ def hello_world():
     return 'Hello, NEU!'
 
 if __name__ == '__main__':
-    # 服务器上由 supervisor 直接用 python3 运行该文件
     app.run(host='0.0.0.0', port=5000)
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,17 +2,20 @@ import unittest
 import sys
 from pathlib import Path
 
-sys.path.append(str(Path(__file__).parent.parent))
+# 将项目根目录添加到Python搜索路径
+sys.path.append(str(Path(__file__).parent.parent))  # 指向flask-web-app目录
 from src.app import app
 
 class TestApp(unittest.TestCase):
     def setUp(self):
+        # 创建测试客户端
         self.client = app.test_client()
 
     def test_hello_world(self):
+        # 测试根路由
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), 'Hello, World!')
+        self.assertEqual(response.data.decode('utf-8'), 'Hello, NEU!')
 
 if __name__ == '__main__':
     unittest.main()
