4d8bdd688ec21ff12e13ef0fb719c6d04ed5cf26
yu-2-tang <1926651882@qq.com>
2025-09-26 15:19:15 +0800

    change to Hello, NEU! and add CI/CD workflow

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, NEU!'
 
 if __name__ == '__main__':
+    # 服务器上由 supervisor 直接用 python3 运行该文件
     app.run(host='0.0.0.0', port=5000)
